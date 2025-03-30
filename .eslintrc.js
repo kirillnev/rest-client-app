26bb91c85c5c91a7f9fdf0212158dc95c4ba1871
Kirill Nevinskiy <k.nevinskiy@tbank.ru>
2025-03-30 19:18:13 +0500

    chore: setup project with Next.js, TypeScript, ESLint and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'react-hooks',
+    'react-refresh',
+    'prettier',
+    'react-compiler',
+    '@next/next',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/strict',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'plugin:@next/next/recommended',
+  ],
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'react-compiler/react-compiler': 'error',
+    'react/react-in-jsx-scope': 'off',
+  },
+  ignorePatterns: ['dist', '.eslintrc.js'],
+};
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>REST Client App </h1>
+    </main>
+  );
+}
