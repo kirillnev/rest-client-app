a279d2c2e83455139ef011139f3a5c61ba75051d
Kirill Nevinskiy <k.nevinskiy@tbank.ru>
2025-04-09 08:15:32 +0500

    refactor: split RequestForm into subcomponents

---
+++ src/components/RequestForm/HeadersBlock.tsx
@@ -0,0 +1,45 @@
+import { FieldErrors, UseFormRegister } from 'react-hook-form';
+import { RestRequest } from '@/types';
+import {
+  FieldArrayWithId,
+  UseFieldArrayAppend,
+  UseFieldArrayRemove,
+} from 'react-hook-form';
+
+type Props = {
+  register: UseFormRegister<RestRequest>;
+  errors: FieldErrors<RestRequest>;
+  fields: FieldArrayWithId<RestRequest, 'headers', 'id'>[];
+  append: UseFieldArrayAppend<RestRequest, 'headers'>;
+  remove: UseFieldArrayRemove;
+};
+
+const HeadersBlock = ({ register, errors, fields, append, remove }: Props) => (
+  <div className="headers-block">
+    {fields.map((field, index) => (
+      <div key={field.id} className="header-wrapper">
+        <div className="header-row">
+          <input {...register(`headers.${index}.key`)} placeholder="Key" />
+          <input {...register(`headers.${index}.value`)} placeholder="Value" />
+          <button
+            type="button"
+            onClick={() => remove(index)}
+            className="icon-button remove"
+          />
+        </div>
+        {errors.headers?.[index]?.key && (
+          <span className="error">{errors.headers[index].key?.message}</span>
+        )}
+      </div>
+    ))}
+    <button
+      type="button"
+      onClick={() => append({ key: '', value: '' })}
+      className="add-link"
+    >
+      Add header
+    </button>
+  </div>
+);
+
+export default HeadersBlock;
--- src/components/RequestForm/RequestForm.tsx
+++ src/components/RequestForm/RequestForm.tsx
@@ -1,6 +1,11 @@
+'use client';
+
 import { UseFormReturn } from 'react-hook-form';
 import { RestRequest } from '@/types';
 import { useRequestForm } from './hooks/useRequestForm';
+import MethodUrlBlock from './MethodUrlBlock';
+import HeadersBlock from './HeadersBlock';
+import BodyBlock from './BodyBlock';
 
 type Props = {
   form: UseFormReturn<RestRequest>;
@@ -9,93 +14,27 @@ type Props = {
 };
 
 const RequestForm = ({ form, onSubmit, isLoading }: Props) => {
-  const { register } = form;
+  const { register, formState, handleSubmit } = form;
   const { fields, append, remove, watchedBodyType, handlePrettify } =
     useRequestForm(form);
 
   return (
-    <form onSubmit={form.handleSubmit(onSubmit)} className="rest-client-form">
+    <form onSubmit={handleSubmit(onSubmit)} className="rest-client-form">
       <fieldset disabled={isLoading}>
-        <div className="method-url-wrapper">
-          <div className="method-url-row">
-            <select {...register('method')}>
-              <option value="GET">GET</option>
-              <option value="POST">POST</option>
-              <option value="PUT">PUT</option>
-              <option value="DELETE">DELETE</option>
-              <option value="PATCH">PATCH</option>
-            </select>
-            <input {...register('url')} placeholder="Enter URL" />
-          </div>
-          {form.formState.errors.url && (
-            <span className="error">{form.formState.errors.url.message}</span>
-          )}
-        </div>
-
-        <div className="headers-block">
-          {fields.map((field, index) => (
-            <div key={field.id} className="header-wrapper">
-              <div className="header-row">
-                <input
-                  {...register(`headers.${index}.key`)}
-                  placeholder="Key"
-                />
-                <input
-                  {...register(`headers.${index}.value`)}
-                  placeholder="Value"
-                />
-                <button
-                  type="button"
-                  onClick={() => remove(index)}
-                  className="icon-button remove"
-                />
-              </div>
-              {form.formState.errors.headers?.[index]?.key && (
-                <span className="error">
-                  {form.formState.errors.headers[index].key?.message}
-                </span>
-              )}
-            </div>
-          ))}
-
-          <button
-            type="button"
-            onClick={() => append({ key: '', value: '' })}
-            className="add-link"
-          >
-            Add header
-          </button>
-        </div>
-
-        <div className="body-type-row">
-          <label>
-            <input
-              type="radio"
-              value="text"
-              {...register('bodyType')}
-              defaultChecked
-            />
-            Text
-          </label>
-          <label>
-            <input type="radio" value="json" {...register('bodyType')} />
-            JSON
-          </label>
-
-          {watchedBodyType === 'json' && (
-            <button type="button" onClick={handlePrettify}>
-              Prettify
-            </button>
-          )}
-        </div>
-
-        <div className="body-row">
-          <textarea {...register('body')} placeholder="Request Body" />
-          {form.formState.errors.body && (
-            <span className="error">{form.formState.errors.body.message}</span>
-          )}
-        </div>
-
+        <MethodUrlBlock register={register} errors={formState.errors} />
+        <HeadersBlock
+          register={register}
+          errors={formState.errors}
+          fields={fields}
+          append={append}
+          remove={remove}
+        />
+        <BodyBlock
+          register={register}
+          errors={formState.errors}
+          watchedBodyType={watchedBodyType}
+          handlePrettify={handlePrettify}
+        />
         <button type="submit">{isLoading ? 'Sending...' : 'Send'}</button>
       </fieldset>
     </form>
