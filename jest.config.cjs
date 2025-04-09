1693f2ae331b576d6e60ef7b4036f6b78f393112
Kirill Nevinskiy <k.nevinskiy@tbank.ru>
2025-04-09 11:22:06 +0500

    chore: change config

--- jest.config.cjs
+++ jest.config.cjs
@@ -5,11 +5,22 @@ const createJestConfig = nextJest({
 });
 
 const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testEnvironment: 'jest-environment-jsdom',
+
+  transform: {
+    '^.+\\.(ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+  },
+
+  transformIgnorePatterns: [
+    '/node_modules/(?!uuid|postman-collection|postman-code-generators)/',
+  ],
+
   moduleNameMapper: {
+    '^uuid$': require.resolve('uuid'),
     '^@/(.*)$': '<rootDir>/src/$1',
   },
+
   collectCoverage: true,
   collectCoverageFrom: [
     'src/**/*.{ts,tsx}',
