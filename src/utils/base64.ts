d62f487803f5531b3c3362e49aaa3379b1c74544
Kirill Nevinskiy <k.nevinskiy@tbank.ru>
2025-04-09 22:44:27 +0500

    refactor: new base64 methods

--- src/utils/base64.ts
+++ src/utils/base64.ts
@@ -1,21 +1,4 @@
-export const encodeBase64 = (str: string): string => {
-  if (typeof window === 'undefined') {
-    return Buffer.from(str, 'utf-8').toString('base64');
-  }
-  const bytes = new TextEncoder().encode(str);
-  const binary = String.fromCharCode(...bytes);
-  return btoa(binary);
-};
+import { Base64 } from 'js-base64';
 
-export const decodeBase64 = (str: string): string => {
-  try {
-    if (typeof window === 'undefined') {
-      return Buffer.from(str, 'base64').toString('utf-8');
-    }
-    const binary = atob(str);
-    const bytes = Uint8Array.from(binary, (c) => c.charCodeAt(0));
-    return new TextDecoder().decode(bytes);
-  } catch {
-    return '';
-  }
-};
+export const encodeBase64 = (str: string): string => Base64.encode(str);
+export const decodeBase64 = (str: string): string => Base64.decode(str);
